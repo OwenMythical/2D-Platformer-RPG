e8c8840f73b134e705b8f3c45f39f17cdf3fb718
Owen Smith <562146@escg.ac.uk>
2025-10-16 14:43:48 +0100

    v11

--- Assets/Scripts/SlimeJump.cs
+++ Assets/Scripts/SlimeJump.cs
@@ -1,41 +1,59 @@
 using Unity.VisualScripting;
 using System.Collections;
 using UnityEngine;
+using System;
 
 public class SlimeJump : MonoBehaviour
 {
     public Rigidbody2D RB;
     public Animator Anim;
     public Transform groundCheck;
-    bool isGrounded;
+    public Transform Player;
+    public BoxCollider2D Collider;
+    bool isGrounded = true;
     private void Awake()
     {
-        WaitForGround();
+        
     }
 
     void Update()
     {
         if (isGrounded == true)
         {
-            Jump(5);
+            isGrounded = false;
+            StartCoroutine(Jump());
         }
     }
 
-    IEnumerator Jump(float Time)
+    IEnumerator Jump()
     {
         yield return new WaitForSeconds(5f);
         Anim.SetBool("Jumping", true);
-        yield return new WaitForSeconds(0.4f);
+        yield return new WaitForSeconds(1);
+        Collider.excludeLayers = LayerMask.GetMask("Player");
         RB.AddForce(transform.up * 8, ForceMode2D.Impulse);
-        WaitForGround();
+        float DistanceX = Player.position.x - transform.position.x;
+        float DistanceY = Player.position.y - transform.position.y;
+        if (Math.Abs(DistanceX) < 5 && Math.Abs(DistanceY) < 5)
+        {
+            RB.AddForce(transform.right * DistanceX, ForceMode2D.Impulse);
+            if (DistanceX < 0)
+            {
+            }
+            else if (DistanceX > 0)
+            {
+            }
+        }
+        yield return new WaitForSeconds(0.5f);
+        StartCoroutine(WaitForGround());
     }
 
     IEnumerator WaitForGround()
     {
-        while (isGrounded == false)
+        bool Found = false;
+        while (Found == false)
         {
             Collider2D[] colliders = Physics2D.OverlapBoxAll(groundCheck.position, groundCheck.localScale, 0f);
-            bool Found = false;
             foreach (Collider2D collider in colliders)
             {
                 if (collider.name == "Collide")
@@ -48,7 +66,7 @@ public class SlimeJump : MonoBehaviour
             {
                 isGrounded = true;
                 Anim.SetBool("Jumping", false);
-                Debug.Log("hit");
+                Collider.excludeLayers = LayerMask.GetMask("Nothing");
             }
             yield return new WaitForSeconds(0.1f);
         }
