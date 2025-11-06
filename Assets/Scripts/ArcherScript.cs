63d87541b1484ef09d5e87fa810d9eb39fdcc352
Owen Smith <562146@escg.ac.uk>
2025-11-06 14:33:22 +0000

    more

--- Assets/Scripts/ArcherScript.cs
+++ Assets/Scripts/ArcherScript.cs
@@ -1,16 +1,46 @@
 using UnityEngine;
+using System.Collections;
 
 public class ArcherScript : MonoBehaviour
 {
     public GameObject Arrow;
     public Rigidbody2D ArrowRB;
+    System.Random RNG = new();
+    public Rigidbody2D RB;
+    public Animator Anim;
+    public BoxCollider2D Collider;
+    public SpriteRenderer SRenderer;
+    public Transform AggroColl;
+    int Aggro = 0;
 
-    void Start()
-    {
-        
-    }
     void Update()
     {
-        
+        bool Found = false;
+        Collider2D[] colliders = Physics2D.OverlapBoxAll(AggroColl.position, AggroColl.localScale, 0f);
+        foreach (Collider2D collider in colliders)
+        {
+            if (collider.gameObject.name == "Player")
+            {
+                Found = true;
+                break;
+            }
+        }
+
+        if (Found == true)
+        {
+            Aggro += 1;
+        }
+        else
+        {
+            Aggro -= 1;
+        }
+    }
+
+    IEnumerator Fire()
+    {
+        Anim.SetBool("AggroStart", true);
+        yield return new WaitForSeconds(0.5f);
+        Anim.SetBool("Chasing", true);
+        Anim.SetBool("AggroStart", false);
     }
 }
